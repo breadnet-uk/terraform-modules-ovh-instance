e6319e9cdc4eccc61dde16bbed456cbe41d20321
Bradley Jenkins <bradley@breadnet.co.uk>
2021-05-03 18:37:40 +0100

    removed borked vars

--- variables.tf
+++ variables.tf
@@ -1,11 +1,7 @@
 variable "sg" {
   type = list
-  default =
-    [openstack_compute_secgroup_v2.ssh.id,
-    openstack_compute_secgroup_v2.icmp.id]
 }
 
 variable "name" {
   type = string
-  default = "instance"
 }
